43cbf0d556d7462a2a50d1cc130bcffb0acb2266
Célita Morales <cmoralesm37@miumg.edu.gt>
2021-08-12 05:25:11 -0600

    terminado... por ahora

--- src/AdventureGame/Heroe.java
+++ src/AdventureGame/Heroe.java
@@ -2,34 +2,53 @@ package AdventureGame;
 
 import jdk.swing.interop.SwingInterOpUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Heroe extends Personaje {
 
 
 
-    private List<Mueble> mochila;
+    private ArrayList<Mueble> mochila = new ArrayList<Mueble>();
 
     public Heroe() {
 
     }
 
-
+    public void setMochila(ArrayList<Mueble> mochila) {
+        this.mochila = mochila;
+    }
 
     public List<Mueble> getMochila() {
         return mochila;
     }
 
-    public void setMochila(List<Mueble> mochila) {
-        this.mochila = mochila;
-    }
 
-    public Heroe(List<Mueble> mochila) {
-        this.mochila = mochila;
-    }
+
+
 //MÉTODOS PARA EL HÉROE
     public void Guardar(Mueble mueble){
-        mochila.add(mueble);
+
+        int tamaño = mochila.size();
+        if(tamaño < 3){
+
+            mochila.add(mueble);
+            int i = getNivelEnergia();
+            int j = getCapacidadAtaque();
+            int mas = i + 4;
+            int masj = j + 4;
+            setCapacidadAtaque(masj);
+            setNivelEnergia(mas);
+
+        }
+        else{
+            System.out.println("Your bag is full");
+        }
+        //entry point:
+        // Mueble gun = new Arma();
+        //heroe.Guardar(gun);
+
+
     }
 
     public void EscalarMuro(){
@@ -38,17 +57,87 @@ public class Heroe extends Personaje {
         setNivelEnergia(menosenergia);
     }
 
+    public void EscalarTorre(){
+        int j =getCapacidadAtaque();
+        int mascapacidad = j + 5;
+        setCapacidadAtaque(mascapacidad);
+    }
+
+    public void Arbol(){
+        int a = getNivelEnergia();
+        int mase = a + 2;
+        setNivelEnergia(mase);
+        int b = getCapacidadAtaque();
+        int menosa = b - 3;
+        setCapacidadAtaque(menosa);
+
+    }
+
+    //public void Pistola(){
+    //        int p = getCapacidadAtaque();
+    //        int masc = p + 5;
+    //        setCapacidadAtaque(masc);
+    //    }
+
+
+   // public void Filete(){
+    //        System.out.println("Save that piece of Steak you just found for later ...");
+    //        int c = getCapacidadAtaque();
+    //        int mas = c + 5;
+    //        int e = getNivelEnergia();
+    //        int masen = e + 5;
+    //        setCapacidadAtaque(mas);
+    //        setNivelEnergia(masen);
+    //        System.out.println("");
+    //    }
+
+    //public void Balas(){
+    //        System.out.println("What a luck that you found an ammunition package... save it for later");
+    //
+    //        int b = getCapacidadAtaque();
+    //        int masb = b + 3;
+    //        int b2 = getNivelEnergia();
+    //        int masb2 = b2 + 5;
+    //        setCapacidadAtaque(masb);
+    //        setNivelEnergia(masb2);
+    //    }
+
+    public void Enemy(){
+        System.out.println("Good Heavens!... Thanos appeared from another dimension ... He's not even part of this franchise, what a crazy interdimensional deranged mother fu... attack him now!");
+        int result = getCapacidadAtaque();
+        if(result >= 10){
+            System.out.println("You defeated him! Now, keep moving forward asap...");
+            int x = result - 10;
+            setCapacidadAtaque(x);
+
+
+        }
+        else{
+            System.out.println("You lose...");
+
+
+        }
+    }
+
     public void Status(){
         System.out.println("");
         System.out.println("---------  CURRENT STATUS  ---------");
 
         int energia = getNivelEnergia();
         int vidas = getVidas();
+        int bag = mochila.size();
         System.out.println("Name: Hector von Humboldt");
         System.out.println("Energy: " + energia);
         System.out.println("Lives: " + vidas);
+        System.out.println("Ability to Attack: "+getCapacidadAtaque());
+        System.out.println("objects in your bag: "+ bag);
+        System.out.println("Current position: ("+getX()+","+getY()+")");
         System.out.println("");
     }
+
+
+
+
 
 
 
--- src/AdventureGame/ep.java
+++ src/AdventureGame/ep.java
@@ -1,7 +1,10 @@
 package AdventureGame;
 
 
+import jdk.jshell.Snippet;
+
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ep {
 
@@ -9,7 +12,8 @@ public class ep {
         Scanner scn = new Scanner(System.in);
         boolean opciones = false, eleccion = false;
 
-        int portalSalidaX= 9, portalSalidaY = 4;
+        int portalSalidaX= 2, portalSalidaY = 5;
+        int cont = 0;
 
 
 
@@ -18,36 +22,32 @@ public class ep {
 
         //Definición de objetos valores para coordenadas dentro del juego
         //INMUEBLES:
-        Muro muro1 = new Muro(); muro1.setX(2); muro1.setY(4);
-        Muro muro2 = new Muro(); muro2.setX(6); muro2.setY(8);
+        Muro muro1 = new Muro(); muro1.setX(5); muro1.setY(4);
+        Muro muro2 = new Muro(); muro2.setX(4); muro2.setY(5);
 
         Torre torre1 = new Torre(); torre1.setX(1); torre1.setY(3);
         Torre torre2 = new Torre(); torre2.setX(3); torre2.setY(1);
 
-        Arbol arbol1 = new Arbol(); arbol1.setX(9); arbol1.setY(1);
-        Arbol arbol2 = new Arbol(); arbol2.setX(8); arbol2.setY(2);
+        Arbol arbol1 = new Arbol(); arbol1.setX(2); arbol1.setY(4);
+        Arbol arbol2 = new Arbol(); arbol2.setX(4); arbol2.setY(3);
         //MUEBLES:
         Alimento filete1 = new Alimento(); filete1.setX(5); filete1.setY(5);
-        Alimento filete2 = new Alimento(); filete2.setX(6); filete2.setY(6);
+        Alimento filete2 = new Alimento(); filete2.setX(1); filete2.setY(2);
 
-        Proyectil balas1 = new Proyectil(); balas1.setX(8); balas1.setY(2);
-        Proyectil balas2 = new Proyectil(); balas2.setX(2); balas2.setY(9);
+        Proyectil balas1 = new Proyectil(); balas1.setX(2); balas1.setY(1);
+        Proyectil balas2 = new Proyectil(); balas2.setX(3); balas2.setY(2);
 
         Arma pistola1 = new Arma(); pistola1.setX(1); pistola1.setY(2);
-        Arma pistola2 = new Arma(); pistola2.setX(10); pistola2.setY(10);
+        Arma pistola2 = new Arma(); pistola2.setX(5); pistola2.setY(3);
         //ENEMIGOS:
-        Enemigo e1 = new Enemigo(); e1.setX(10); e1.setY(4); e1.setNivelEnergia(1); e1.setVidas(2); e1.setCapacidadAtaque(10);
-        Enemigo e2 = new Enemigo(); e2.setX(10); e2.setY(10); e2.setNivelEnergia(1); e2.setNivelEnergia(10); e2.setVidas(2); e2.setCapacidadAtaque(10);
+        Enemigo e1 = new Enemigo(); e1.setX(3); e1.setY(3); e1.setNivelEnergia(1); e1.setVidas(1); e1.setCapacidadAtaque(12);
+        Enemigo e2 = new Enemigo(); e2.setX(4); e2.setY(1); e2.setNivelEnergia(1); e2.setNivelEnergia(10); e2.setVidas(1); e2.setCapacidadAtaque(12);
         //HÉROE:
         Heroe heroe = new Heroe();
 
-        heroe.setNivelEnergia(8); heroe.setVidas(2); heroe.setCapacidadAtaque(10);
+        heroe.setNivelEnergia(8); heroe.setVidas(1); heroe.setCapacidadAtaque(10);
 
-
-
-
-        //MÉTODOS
-
+        ArrayList<Mueble> bag = new ArrayList<Mueble>();
 
 
 
@@ -65,12 +65,13 @@ public class ep {
 
             switch(op){
                 case 1:
-                    while(eleccion == false){
-                        System.out.println("Welcome, Hector. Are you a hero? Choose your coordinates to Advance within the field");
 
+                        System.out.println("Welcome, Hector. Are you a hero? Choose your coordinates to Advance within the field");
+                    while(eleccion == false){
 
                         heroe.Status();
-                        System.out.println("Choose coordinates from 1 to 10 ");
+
+                        System.out.println("Choose coordinates only between 1 to 5");
                         System.out.println("Denote coordinate X: ");
                         int xh;
                         xh = scn.nextInt();
@@ -80,18 +81,86 @@ public class ep {
                         heroe.setX(xh); heroe.setY(yh);
                         
                         System.out.println("");
-
-                        if((heroe.getX() == muro1.getX() && heroe.getY() == muro1.getY()) || heroe.getX()== muro2.getX() && heroe.getY()== muro2.getY()){
+                        if(heroe.getX() == portalSalidaX && heroe.getY() == portalSalidaY){
+                            System.out.println("");
+                            System.out.println("Hurry up! is time to escape through the hatch. ");
+                            System.out.println("Congratulations, you advanced to the next level !!");
+                            System.out.println("\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6"+"\uD83C\uDFC6");
+                            eleccion = true;
+                        }
+                        else if((heroe.getX() == muro1.getX() && heroe.getY() == muro1.getY()) || heroe.getX()== muro2.getX() && heroe.getY()== muro2.getY()){
+                            System.out.println("");
                             System.out.println("You must climb the wall to continue. ENERGY: -2");
                             heroe.EscalarMuro();
                             heroe.Status();
+                            System.out.println("Try again to advance... ");
+                            eleccion = false;
+                        }
+                        else if((heroe.getX() == torre1.getX() && heroe.getY() == torre1.getY()) || heroe.getX() == torre2.getX() && heroe.getY() == torre2.getY()){
+                            System.out.println("");
+                            System.out.println("You found a Tower. Climb to the top to observe the position of your enemies and threats... Ability to Attack: +5");
+                            heroe.EscalarTorre();
+                            heroe.Status();
+                            System.out.println("Try again to advance...");
+                            eleccion = false;
 
+                        }
+                        else if((heroe.getX() == arbol1.getX() && heroe.getY() == arbol1.getY()) || heroe.getX() == arbol2.getX() && heroe.getY() == arbol2.getY()) {
+
+
+
+
+                            System.out.println("you were lucky to evade a sentry attack by climbing the tree. Unfortunately he hit you on the arm. Rest for a moment on the top of the tree to regain energy.");
+                            System.out.println("Energy: +2");
+                            System.out.println("Hability to Attack: -3");
+                            heroe.Arbol();
+                            heroe.Status();
+                            System.out.println("Try again to advance...");
+                            eleccion = false;
+                                }
+                        else if((heroe.getX() == pistola1.getX() && heroe.getY() == pistola1.getY()) || heroe.getX() == pistola2.getX() && heroe.getY() == pistola2.getY()){
+
+                            System.out.println("you found a gun. Save it if you can. You'll have to use it soon...");
+                            Mueble weapon = new Arma();
+                            heroe.Guardar(weapon);
+                            heroe.Status();
+                            System.out.println("Try again to advance...");
+                            eleccion = false;
+
+                        }
+                        else if((heroe.getX() == filete1.getX() && heroe.getY() == filete1.getY()) || (heroe.getX() == filete2.getX() && heroe.getY() == filete2.getY())) {
+                            Mueble alimento = new Alimento();
+                            heroe.Guardar(alimento);
+                            heroe.Status();
+                            System.out.println("Try again to advance...");
+                            eleccion = false;
+
+
+                        }
+                        else if((heroe.getX() == balas1.getX() && heroe.getY() == balas1.getY()) ||  (balas2.getX() == filete2.getX() && heroe.getY() == balas2.getY())){
+
+
+                                Mueble projectile = new Proyectil();
+                                heroe.Guardar(projectile);
+                                heroe.Status();
+                                System.out.println("Try again to advance...");
+                                eleccion = false;
+
+                            }
+
+                        else if( (heroe.getX() == e1.getX() && heroe.getY() == e1.getY()) ||  (heroe.getX() == e2.getX() && heroe.getY() == e2.getY())){
+                                heroe.Enemy();
+                                heroe.Status();
+                            System.out.println("Try again to advance...");
+                            eleccion = false;
+                        }
+                        else{
+                            System.out.println("You moved to position ("+heroe.getX()+","+heroe.getY()+")");
+                            eleccion = false;
                         }
 
 
 
-
-                        eleccion = true;
 
 
                     }
